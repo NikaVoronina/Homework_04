2600c789acb0fdebdb7b4ca624f658fc80519f2d
Ника Воронина <nika_voronina_95@mail.ru>
2022-08-17 18:51:03 +0300

    Task_1 added

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -5,3 +5,15 @@
 // 3, 5 -> 243 (3⁵)
 
 // 2, 4 -> 16
+
+
+Console.Write("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+int degree = A;
+for (int i = 1; i < B; i++)
+{
+    degree = degree * A;
+}
+Console.WriteLine(degree);
