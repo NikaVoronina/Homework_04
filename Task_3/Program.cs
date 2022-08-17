b9c88bf3fa9f721c476996d1d93b0ecbc2bc7aed
Ника Воронина <nika_voronina_95@mail.ru>
2022-08-17 18:44:39 +0300

    Task_3 added

--- Task_3/Program.cs
+++ Task_3/Program.cs
@@ -5,3 +5,18 @@
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 
 // 6, 1, 33 -> [6, 1, 33]
+
+
+int []array = new int[8];
+Console.Write("Массив из 8 элементов:");
+
+for (int i = 0; i < array.Length; i++)
+ {
+    array [i] = new Random().Next(0, 8);
+    Console.Write(" " + Method (i) + " ");
+ }
+
+int Method (int a)
+{
+    return array[a];
+}
